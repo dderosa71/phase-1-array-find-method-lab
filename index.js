c555c6a2bd780dec76fba53f71a59aa4643962bf
Dominic DeRosa <dderosa71@gmail.com>
2021-07-15 16:02:14 -0500

    commit tree 21aac93e40ac5914daffed7a93895fe82104094b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+function finder(array){
+    return array.find(obj => obj.result ==='W') 
+}
+function superbowlWin(arr){
+    let winFound = finder(arr)
+    return winFound ? winFound.year : undefined;
+}
